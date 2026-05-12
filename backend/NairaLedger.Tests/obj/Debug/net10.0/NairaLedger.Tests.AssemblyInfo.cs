430b018bf17215c8017d127612567e5c09453037
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-05-12 14:01:17 +0100

    test(domain): add Wallet aggregate unit tests

--- backend/NairaLedger.Tests/obj/Debug/net10.0/NairaLedger.Tests.AssemblyInfo.cs
+++ backend/NairaLedger.Tests/obj/Debug/net10.0/NairaLedger.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NairaLedger.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fd48ad51c4a4d9f7f1443936afb980c324a4017")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74890d6975ba8c5f554cc1f5227c3f7b8bf12b07")]
 [assembly: System.Reflection.AssemblyProductAttribute("NairaLedger.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NairaLedger.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
