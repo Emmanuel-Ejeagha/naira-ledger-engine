1420be975a87f2de1bc49f6ac352073b1e08ff38
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-05-15 13:41:45 +0100

    feat(application): add domain event notifications and handlers

---
+++ backend/NairaLedger.Application/Commands/EventHandlers/DomainEventNotification.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using NairaLedger.Domain.DomianEvents;
+
+namespace NairaLedger.Application.Commands.EventHandlers;
+
+public record DomainEventNotification<T>(T DomainEvent) : INotification
+    where T : IDomainEvent;
