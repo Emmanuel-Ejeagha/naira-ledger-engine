9aa6810b276aa1af8d1d44b6cab46a1b30a0a6d7
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-05-21 15:26:28 +0100

    feat(api): add authentication endpoint

---
+++ backend/NairaLedger.WebApi/Endpoints/AuthEndpoints.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using NairaLedger.Application.Commands.Auth;
+using NairaLedger.Application.Commands.Auth.Register;
+using NairaLedger.Application.Commands.Auth.Token;
+
+namespace NairaLedger.WebApi.Endpoints;
+
+public static class AuthEndpoints
+{
+    public static void MapAuthEndpoints(this WebApplication app)
+    {
+        app.MapPost("/api/v1/auth/register", async (RegisterUserCommand command, IMediator mediator) =>
+        {
+            var result = await mediator.Send(command);
+            return Results.Ok(result);
+        }).RequireRateLimiting("strict");
+
+        app.MapPost("/api/v1/auth/login", async (LoginUserCommand command, IMediator mediator) =>
+        {
+            var result = await mediator.Send(command);
+            return Results.Ok(result);
+        }).RequireRateLimiting("strict");
+
+        app.MapPost("/api/v1/auth/refresh", async (RefreshTokenCommand command, IMediator mediator) =>
+        {
+            var result = await mediator.Send(command);
+            return Results.Ok(result);
+        }).RequireRateLimiting("strict");
+    }
+}       
