dff34ab68c4a15dc7309d58b86314a6e24b0c313
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-05-24 15:40:22 +0100

    chore(infrastructure): add initial database migrations

---
+++ backend/NairaLedger.Infrastructure/Migrations/20260522185021_FixWalletVersion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NairaLedger.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixWalletVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
