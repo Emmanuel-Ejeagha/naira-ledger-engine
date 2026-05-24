8f15c9f17c686be2c4e6c6476270d12e0a6cc8dc
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-05-24 15:52:39 +0100

    feat(api): add AuthorizeCheckOperation for access control

---
+++ backend/NairaLedger.WebApi/Authorization/AuthorizeCheckOperationFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace NairaLedger.WebApi.Authorization;
+
+public class AuthorizeCheckOperationFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        var hasAuthorize = context.MethodInfo.DeclaringType!.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any()
+                           || context.MethodInfo.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any();
+
+        if (!hasAuthorize) return;
+
+        operation.Security = new List<OpenApiSecurityRequirement>
+        {
+            new OpenApiSecurityRequirement
+            {
+               [new OpenApiSecuritySchemeReference("Bearer")] = new List<string>()
+            }
+        };
+    }
+}
