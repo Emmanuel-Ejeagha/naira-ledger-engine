b1e05a6d63ab44345a43e3c7a749720c5fc7c0a9
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-06-20 12:38:47 +0100

    feat(application): implement ResetPasswod command and handler

---
+++ backend/NairaLedger.Application/Commands/Auth/ResetPasswd/ResetPasswordCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace NairaLedger.Application.Commands.Auth.ResetPasswd;
+
+public record ResetPasswordCommand(Guid UserId, string Token, string NewPassword, string Email) : IRequest<Unit>;
---
+++ backend/NairaLedger.Application/Commands/Auth/ResetPasswd/ResetPasswordCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NairaLedger.Application.Commands.Auth.ResetPasswd;
+
+public class ResetPasswordCommandHandler : IRequestHandler<ResetPasswordCommand, Unit>
+{
+    private readonly IUserService _userService;
+    private readonly ILogger<ResetPasswordCommandHandler> _logger;
+
+    public ResetPasswordCommandHandler(IUserService userService, ILogger<ResetPasswordCommandHandler> logger)
+    {
+        _userService = userService;
+        _logger = logger;
+    }
+
+    public async Task<Unit> Handle(ResetPasswordCommand request, CancellationToken cancellationToken)
+    {
+        var decodedToken = Uri.UnescapeDataString(request.Token);
+        await _userService.ResetPasswordAsync(request.UserId, decodedToken, request.NewPassword, cancellationToken);
+        _logger.LogInformation("Password reset successful for user {UserId}", request.UserId);
+        return Unit.Value;
+    }
+}
