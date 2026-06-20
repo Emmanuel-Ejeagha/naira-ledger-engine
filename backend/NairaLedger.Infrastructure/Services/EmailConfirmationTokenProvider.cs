de13465c9eb2f5b173e05f1996017a81a67ce760
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-06-20 11:39:09 +0100

    feat(auth): improve email confirmation token provider and email sending services

---
+++ backend/NairaLedger.Infrastructure/Services/EmailConfirmationTokenProvider.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+
+namespace NairaLedger.Infrastructure.Services;
+
+public class EmailConfirmationTokenProvider<TUser> : DataProtectorTokenProvider<TUser>
+    where TUser : class
+{
+    public EmailConfirmationTokenProvider(IDataProtectionProvider dataProtectionProvider,
+        IOptions<DataProtectionTokenProviderOptions> options, ILogger<DataProtectorTokenProvider<TUser>> logger)
+        : base(dataProtectionProvider, options, logger)
+    {
+    }
+}
